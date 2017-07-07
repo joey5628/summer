0749c6d970dce8e4f9f262b3e9a491e21cf39c12
zhangyi <zhangyi5628@126.com>
2017-07-07 18:42:52 +0800

    progress

--- src/components/Progress/index.js
+++ src/components/Progress/index.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react'
 import {
     View,
 } from 'react-native'
+import styles from './styles'
 
 export default class Progress extends Component {
     constructor (props) {
         super(props)
     }
+
+    static defaultProps = {
+        percent: 0,
+        unfilled: 'show',
+        appearTransition: false,
+        styles: styles
+    }
+
+
 
     render () {
         return (
